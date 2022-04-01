2e9244870327beaa75f4d3de9388c19a505e5707
sontre96 <sontre96@gmail.com>
2022-04-01 23:54:47 +0700

    push exam 01/04/2022

---
+++ Gettingstared/Race.java
@@ -0,0 +1,15 @@
+package Exam1.Gettingstared;
+
+public class Race {
+    public static void main(String a[]) {
+        RaceCar[] cars = new RaceCar[5];
+        cars[0] = new RaceCar(10, "Mario");
+        cars[1] = new RaceCar(10, "Songoku");
+        cars[2] = new RaceCar(10, "Herman");
+        cars[3] = new RaceCar(10, "Doremon");
+        cars[4] = new RaceCar(10, "Hoang Phi Hong");
+
+        for (int i =0; i < 5; i++)
+            cars[i].start();
+    }
+}
